994fef6dfdc504bf4e184fc28b17f5353907ba1c
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-12-04 03:20:25 +0100

    Made some changes to the Landing page routes and erro404 page

--- src/views/Error404Page.js
+++ src/views/Error404Page.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Layout, Button, Empty } from 'antd';
+import emptyImage from '../assets/images/undraw_404.svg';
 
-const { Header, Content, Footer } = Layout;
 
 const Error404Page = (props) => {
     console.log(props)
     return (
         <Layout className="layout">
-            {/* <Header>
-                <div className="logo" />
-                <div style={{ lineHeight: '64px', float: 'right' }}>
-                    <Link to='/other'>
-                        <Button type="primary">Login</Button>
-                    </Link>
-                </div>
-            </Header> */}
-            <div style={{ minHeight: 'calc(100vh - 133px)', backgroundColor: 'white' }}>
+            <div style={{ minHeight: 'calc(100vh - 233px)', marginTop: '100px' }}>
                 <Empty
-                    image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
+                    image={emptyImage}
                     imageStyle={{
-                        height: 60,
+                        height: 400,
                     }}
                     description={
-                        <span>
-                            Sorry page not found
-                        </span>
+                        <p className='error404-text'>
+                            Page not found
+                        </p>
                     }
                 >
                     <Button type="primary" onClick={() => {
@@ -33,7 +25,6 @@ const Error404Page = (props) => {
                     }}>Go back</Button>
                 </Empty>
             </div>
-            <Footer style={{ textAlign: 'center' }}>AlgoLearn ©2019 Created by Oreoluwa Bimbo-Salami</Footer>
         </Layout>
     );
 }
